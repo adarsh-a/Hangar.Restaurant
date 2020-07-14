7fe46eeeb4d4af2dfa4e715d0511815354ad581b
Alex LACOUR <alex.lacour@hangarww.com>
2020-07-14 17:08:05 +0400

    add models

---
+++ Models/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Hangar.Restaurant.Models
+{
+    public class Menu
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public bool Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [ForeignKey("MenuType")]
+        public MenuType Type { get; set; }
+    }
+}   
---
+++ Models/MenuType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hangar.Restaurant.Models
+{
+    public class MenuType
+    {
+
+        public string ID { get; set; }
+        public string name { get; set; }
+
+    }
+}
