b609abf89e5c1784d64ddf720b8dce55d994458d
Jigisha Soobdhon <jigisha.soobdhon@hangarww.com>
2020-07-17 13:14:40 +0400

    Changes made

---
+++ Controllers/MenusController.cs
@@ -0,0 +1,22 @@
+﻿using Hangar.Restaurant.Database;
+using Hangar.Restaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hangar.Restaurant.Controllers
+{
+    public class MenusController : Controller
+    {
+        // GET: Menus
+        private RestaurantDbContext db = new RestaurantDbContext();
+        public ActionResult FetchMenu()
+        {
+            var menusModel = new Menus();
+            var menusData = db.Menus.ToList();
+            return View("~/Views/PartialView/Menus.cshtml", menusData);
+        }
+    }
+}
