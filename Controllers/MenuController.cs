a0d0326c9da402e37d7466db7384b83c149edbe8
Alex LACOUR <alex.lacour@hangarww.com>
2020-07-22 14:03:41 +0400

    Menu page linked, ad layout added

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,22 +1,22 @@
 ﻿using Hangar.Restaurant.Contracts;
-using Hangar.Restaurant.Database;
 using Hangar.Restaurant.Database.Models;
 using Hangar.Restaurant.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Hangar.Restaurant.Controllers
 {
-    public class MenuSectionController : Controller
+    public class MenuController : Controller
     {
         private IRepositoryBase<MenuEntity> menuContext;
         private IRepositoryBase<MenuSectionEntity> sectionContext;
         private IRepositoryBase<MenuTypeEntity> typeContext;
 
-        public  MenuSectionController(IRepositoryBase<MenuEntity> menuCon, IRepositoryBase<MenuSectionEntity> secCon,
+        public MenuController(IRepositoryBase<MenuEntity> menuCon, IRepositoryBase<MenuSectionEntity> secCon,
             IRepositoryBase<MenuTypeEntity> typeCon)
         {
             menuContext = menuCon;
@@ -24,43 +24,41 @@ namespace Hangar.Restaurant.Controllers
             typeContext = typeCon;
         }
 
-        // GET: MenuSection
-        public PartialViewResult FetchMenuSection()
+        // GET: Menu
+        public PartialViewResult Index()
         {
-            
-
             MenuSectionEntity menuData = sectionContext.Collection().FirstOrDefault();
             MenuSection section = new MenuSection();
 
-            List<MenuEntity> menus = menuContext.Collection().Include(ent => ent.Type).ToList();
+            List<MenuEntity> menus = menuContext.Collection().Include(ent => ent.Type).OrderBy(p => p.Price).ToList();
             List<Menu> menuListModel = new List<Menu>();
 
             List<MenuTypeEntity> menuTypes = typeContext.Collection().ToList();
-            List <MenuType> menuTypeList= new List<MenuType>();
-   
+            List<MenuType> menuTypeList = new List<MenuType>();
 
-            if (menuData != null) 
+
+            if (menuData != null)
             {
                 section.Title = menuData.Title;
                 section.Description = menuData.Description;
-            
+
             }
 
             foreach (var item in menus)
             {
-                
+
                 menuListModel.Add(new Menu()
                 {
                     Name = item.Name,
                     Description = item.Description,
                     Price = item.Price,
                     Image = item.Image,
-                    Type = new MenuType() { name = item.Type.name.ToLower()}
+                    Type = new MenuType() { name = item.Type.name.ToLower() }
 
                 }); ;
             }
 
-            foreach(var type in menuTypes)
+            foreach (var type in menuTypes)
             {
                 menuTypeList.Add(new MenuType
                 {
@@ -69,18 +67,15 @@ namespace Hangar.Restaurant.Controllers
             }
 
             //define model to display in view
-            SectionVM model = new SectionVM() {
+            SectionVM model = new SectionVM()
+            {
                 menuList = menuListModel,
                 Title = section.Title,
                 Description = section.Description,
                 typeList = menuTypeList
             };
 
-            return PartialView("~/Views/PartialView/Menus.cshtml", model);
+            return PartialView(model);
         }
-
-        
-
-
     }
 }
--- Models/Menu.cs
+++ Models/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -8,16 +9,15 @@ using System.Web;
 namespace Hangar.Restaurant.Models
 {
     public class Menu
-    { 
-        public string ID { get; set; }
+    {
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         [ForeignKey("MenuTypeId")]
         public MenuType Type { get; set; }
+        [DisplayName("Menu Type")]
         public string MenuTypeId { get; set; }
     }
-
     
 }   
