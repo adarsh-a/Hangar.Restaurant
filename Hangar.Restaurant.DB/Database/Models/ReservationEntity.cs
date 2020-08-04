ae598e206446dfa6eac760082b8240d2eace33b5
Alex LACOUR <alex.lacour@hangarww.com>
2020-08-04 11:32:44 +0400

    remove some unwanted comments

--- Hangar.Restaurant.DB/Database/Models/ReservationEntity.cs
+++ Hangar.Restaurant.DB/Database/Models/ReservationEntity.cs
@@ -21,10 +21,5 @@ namespace Hangar.Restaurant.DB.Database.Models
         [ForeignKey("tableId")]
         public TableEntity table { get; set; }
         public int tableId { get; set; }
-
-        //table enity -- id{1-10} name
-        // reservation table
-        // take all rows in this time and date -- (count)check number of reservation >10 all reserved <assign table
-        //get last table id + 1 or skip number of table and asign it to
     }
 }
